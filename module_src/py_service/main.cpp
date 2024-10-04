bef9fbf038b7b2ea25f4d353ce4366de83207f8a
yuyuaqwq <1454832774@qq.com>
2024-10-04 21:52:19 +0800

    .

--- module_src/py_service/main.cpp
+++ module_src/py_service/main.cpp
@@ -6,17 +6,18 @@
 #include <pybind11/pybind11.h>
 namespace py = pybind11;
 
-PYBIND11_EMBEDDED_MODULE(example, m) {
-    m.def("add", [](int a, int b) {
-        return a + b;
-    });
-
-    auto math = m.def_submodule("math");
-}
+//PYBIND11_EMBEDDED_MODULE(example, m) {
+//    m.def("add", [](int a, int b) {
+//        return a + b;
+//    });
+//
+//    auto math = m.def_submodule("math");
+//}
 
 
 MILINET_FUNC_EXPORT bool MiliModuleInit(milinet::IMilinet* imilinet) {
     py_initialize();
+
     bool ok = py_exec("print('Hello world!')", "<string>", EXEC_MODE, NULL);
 
     return true;
