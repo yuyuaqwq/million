f9e269514849134019c0972db300497e23d9a15d
yuyuaqwq <1454832774@qq.com>
2024-10-26 14:38:31 +0800

    build调整

--- build_release.bat
+++ build_release.bat
@@ -1,3 +1,4 @@
+mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=C:/lib/vcpkg/scripts/buildsystems/vcpkg.cmake -Dprotobuf_DIR=C:/lib/vcpkg/installed/x64-windows/share/protobuf -DMYSQL_HEADER_DIR="C:\Program Files\mysql-5.7.44-winx64\include" -DMYSQL_LIB="C:\Program Files\mysql-5.7.44-winx64\lib/libmysql.lib"
 pause
