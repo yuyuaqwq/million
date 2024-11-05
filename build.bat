71cc381ec5db63c9713ed0f0281256eca102e2c9
yuyuaqwq <1454832774@qq.com>
2024-11-05 16:36:17 +0800

    项目结构

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=?/vcpkg/scripts/buildsystems/vcpkg.cmake -Dprotobuf_DIR=?/vcpkg/installed/x64-windows/share/protobuf -DMYSQL_HEADER_DIR="?/mysql-*/include" -DMYSQL_INCLUDE_DIR="?/mysql-*/include" -DMYSQL_LIB="?/mysql-*/lib/libmysql.lib"
+cmake .. -DCMAKE_TOOLCHAIN_FILE=?/vcpkg/scripts/buildsystems/vcpkg.cmake -Dprotobuf_DIR=?/vcpkg/installed/x64-windows/share/protobuf -DMYSQL_HEADER_DIR="?/mysql-*/include" -DMYSQL_LIB="?/mysql-*/lib/libmysql.lib"
 pause
