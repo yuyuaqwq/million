13ccb8fee2987301dae48e1adfaac401a1621b86
yuyuaqwq <1454832774@qq.com>
2024-11-10 23:48:55 +0800

    designer

--- designer/build.bat
+++ designer/build.bat
@@ -3,6 +3,7 @@ set PACKAGE=Desc
 cd table
 ..\tool\tabtoy -mode=v3 -index=Index.xlsx -package=%PACKAGE% -proto_out=../desc/desc.proto
 ..\tool\tabtoy -mode=v3 -index=Index.xlsx -package=%PACKAGE% -pbbin_out=../../descgen/descdata.pbb
+::..\tool\tabtoy -mode=v3 -index=Index.xlsx -package=%PACKAGE% -pbbin_dir=../../descgen/
 
 cd ..
 set OUTPUT_DIR=..\protogen
