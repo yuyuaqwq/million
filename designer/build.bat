edd4d60cbc60a34012ca526b23d6323e8f845ee3
yuyuaqwq <1454832774@qq.com>
2024-11-10 23:33:49 +0800

    add designer

---
+++ designer/build.bat
@@ -0,0 +1,14 @@
+set PACKAGE=Desc
+
+cd table
+..\tool\tabtoy -mode=v3 -index=Index.xlsx -package=%PACKAGE% -proto_out=../desc/desc.proto
+..\tool\tabtoy -mode=v3 -index=Index.xlsx -package=%PACKAGE% -pbbin_out=../../descgen/descdata.pbb
+
+cd ..
+set OUTPUT_DIR=..\protogen
+mkdir %OUTPUT_DIR% 2>nul
+for /d %%D in (*) do (
+    protoc --cpp_out=%OUTPUT_DIR% .\%%D\*.proto
+)
+
+pause
