2991a78a2875562762685bd7dab5d551e186dbb5
yuyuaqwq <1454832774@qq.com>
2024-11-18 16:21:59 +0800

    cmake

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake ..-DPROTOBUF_LIB_DIR=? -DPROTOBUF_INCLUDE_DIR=? -DMYSQL_HEADER_DIR="?/mysql-*/include" -DMYSQL_LIB="?/mysql-*/lib/libmysql.lib"
+cmake .. -DPROTOBUF_LIB_DIR=? -DPROTOBUF_INCLUDE_DIR=? -DMYSQL_HEADER_DIR="?/mysql-*/include" -DMYSQL_LIB="?/mysql-*/lib/libmysql.lib"
 pause
