01d7966036f31f78219ed8fa8c35da3ca25181a7
yuyuaqwq <1454832774@qq.com>
2024-12-09 11:21:00 +0800

    service_lock

--- million/src/service_lock.cpp
+++ million/src/service_lock.cpp
@@ -4,7 +4,7 @@
 
 namespace million {
 
-MILLION_MSG_DEFINE_EMPTY(, MillionServiceLockMsg);
+MILLION_MSG_DEFINE_EMPTY(, MillionServiceUnlockMsg);
 
 ServiceLock::ServiceLock(IService* iservice)
 	: iservice_(iservice)
@@ -17,14 +17,14 @@ Task<> ServiceLock::Lock() {
 	}
 	auto session_id = iservice_->AllocSessionId();
 	wait_sessions_.push(session_id);
-	co_await iservice_->Recv<MillionServiceLockMsg>(session_id);
+	co_await iservice_->Recv<MillionServiceUnlockMsg>(session_id);
 	locking_ = true;
 }
 
 void ServiceLock::Unlock() {
 	locking_ = false;
 	if (!wait_sessions_.empty()) {
-		iservice_->SendTo<MillionServiceLockMsg>(iservice_->service_handle(), wait_sessions_.front());
+		iservice_->SendTo<MillionServiceUnlockMsg>(iservice_->service_handle(), wait_sessions_.front());
 		wait_sessions_.pop();
 	}
 }
