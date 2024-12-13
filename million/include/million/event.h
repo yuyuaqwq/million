4ddd0e77b7efed09097593938d0b685c4f4002e5
yuyuaqwq <1454832774@qq.com>
2024-12-13 19:10:54 +0800

    EventManager

--- million/include/million/event.h
+++ million/include/million/event.h
@@ -4,19 +4,16 @@
 
 #include <unordered_map>
 
-#include <million/api.h>
-#include <million/imsg.h>
-#include <million/iservice.h>
-#include <million/service_handle.h>
+#include <million/imillion.h>
 
 namespace million {
 
 MILLION_MSG_DEFINE(MILLION_API, EventRegister, (const std::type_info&) type)
 
-class EventSubscriber {
+class MILLION_API EventManager {
 public:
-	EventSubscriber(IService* iservice)
-		: iservice_(iservice) {}
+	EventManager(IMillion* imillion)
+		: imillion_(imillion) {}
 
 	void Subscribe(const std::type_info& type, const ServiceHandle& handle) {
 		auto iter = event_map_.find(&type);
@@ -28,7 +25,7 @@ public:
 		iter->second.emplace_back(handle);
 	}
 
-	void Send(MsgUnique msg) {
+	void Send(const ServiceHandle& sender, MsgUnique msg) {
 		auto iter = event_map_.find(&msg->type());
 		if (iter == event_map_.end()) {
 			// 没有关注此事件的服务
@@ -36,7 +33,7 @@ public:
 		}
 		auto& services = iter->second;
 		for (auto service_iter = services.begin(); service_iter != services.end(); ) {
-			auto id = iservice_->Send(*service_iter, MsgUnique(msg->Copy()));
+			auto id = imillion_->Send(sender, *service_iter, MsgUnique(msg->Copy()));
 			if (id == kSessionIdInvalid) {
 				// 已关闭的服务
 				services.erase(service_iter++);
@@ -48,7 +45,7 @@ public:
 	}
 
 private:
-	IService* iservice_;
+	IMillion* imillion_;
 	std::unordered_map<const std::type_info*, std::list<ServiceHandle>> event_map_;
 };
 
