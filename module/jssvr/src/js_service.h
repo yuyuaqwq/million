5d9497f1e67a8514a9319849bbb1b80294cd992d
yuyuaqwq <1454832774@qq.com>
2025-05-22 12:05:28 +0800

    module

--- module/jssvr/src/js_service.h
+++ module/jssvr/src/js_service.h
@@ -36,7 +36,25 @@ struct ServiceFuncContext {
 };
 
 
+class JSService;
+class JSModuleManager : public mjs::ModuleManagerBase {
+public:
+    void AddCppModule(std::string_view path, mjs::CppModuleObject* cpp_module_object) override;
+    mjs::Value GetModule(mjs::Context* ctx, std::string_view path) override;
+    mjs::Value GetModuleAsync(mjs::Context* ctx, std::string_view path) override;
+    void ClearModuleCache() override;
 
+private:
+    // 读取模块脚本
+    std::optional<std::string> ReadModuleScript(const std::filesystem::path& module_path);
+    mjs::Value LoadJSModule(JSService* js_service, std::string_view module_name);
+    mjs::Value FindJSModule(JSService* js_service, std::filesystem::path path);
+
+private:
+    std::vector<std::string> jssvr_dirs_;
+    std::unordered_map<fs::path, mjs::Value> cpp_modules_;
+    std::unordered_map<fs::path, mjs::Value> module_defs_;
+};
 
 // JS运行时服务
 class JSRuntimeService : public IService {
@@ -85,6 +103,7 @@ private:
 
 
 private:
+    friend class JSService;
     mjs::Runtime js_runtime_;
     std::vector<std::string> jssvr_dirs_;
     ServiceHandle db_handle_;
@@ -102,7 +121,10 @@ public:
     using Base = IService;
     JSService(IMillion* imillion, JSRuntimeService* js_runtime_service, const std::string& package)
         : Base(imillion)
-        , js_context_(&js_runtime_service->js_runtime()) {}
+        , js_context_(&js_runtime_service->js_runtime())
+    {
+        js_module_ = mjs::Value(mjs::String::New(package));
+    }
 
 private:
     // 初始化JS运行时和上下文
@@ -122,8 +144,8 @@ private:
         //if (!CreateConfigModule()) {
         //    TaskAbort("CreateConfigModule failed.");
         //}
-
-        js_module_ = LoadModule(package_);
+        auto js_module_name = std::move(js_module_);
+        js_module_ = js_context_.runtime().module_manager().GetModule(&js_context_, js_module_name.string_view());
         if (JSCheckExceptionAndLog(js_module_)) {
             TaskAbort("LoadModule failed with exception.");
         }
@@ -721,12 +743,18 @@ private:
     }
 
 public:
+    JSRuntimeService& js_runtime_service() const { 
+        auto& runtime = js_context_.runtime();
+        size_t offset = offsetof(JSRuntimeService, js_runtime_);
+        auto* js_runtime_service = reinterpret_cast<JSRuntimeService*>(reinterpret_cast<char*>(&runtime) - offset);
+        return *js_runtime_service;
+    }
     auto& js_context() { return js_context_; }
     auto& js_module() { return js_module_; }
     auto& js_module_cache() { return js_module_cache_; }
+
 private:
     friend class JSModuleManager;
-    JSRuntimeService* js_runtime_service_;
 
     // mjs上下文
     mjs::Context js_context_;
@@ -739,109 +767,112 @@ private:
     ServiceFuncContext func_ctx_;
 };
 
-class JSModuleManager : public mjs::ModuleManagerBase {
-public:
-    void AddCppModule(std::string_view path, mjs::CppModuleObject* cpp_module_object) override {
-        cpp_modules_.emplace(path, mjs::Value(cpp_module_object));
+
+
+void JSModuleManager::AddCppModule(std::string_view path, mjs::CppModuleObject* cpp_module_object) {
+    cpp_modules_.emplace(path, mjs::Value(cpp_module_object));
+}
+
+mjs::Value JSModuleManager::GetModule(mjs::Context* ctx, std::string_view path) {
+    auto iter = cpp_modules_.find(path);
+    if (iter != cpp_modules_.end()) {
+        return iter->second;
     }
+    size_t offset = offsetof(JSService, js_context_);
+    auto* js_service = reinterpret_cast<JSService*>(reinterpret_cast<char*>(ctx) - offset);
+    return LoadJSModule(js_service, path);
+}
 
-    mjs::Value GetModule(mjs::Context* ctx, std::string_view path) override {
-        auto iter = cpp_modules_.find(path);
-        if (iter != cpp_modules_.end()) {
-            return iter->second;
-        }
-        size_t offset = offsetof(JSService, js_context_);
-        auto* js_service = reinterpret_cast<JSService*>(reinterpret_cast<char*>(ctx) - offset);
-        return LoadJSModule(js_service, path);
+mjs::Value JSModuleManager::GetModuleAsync(mjs::Context* ctx, std::string_view path) {
+    return GetModule(ctx, path);
+}
+
+void JSModuleManager::ClearModuleCache() {
+    module_defs_.clear();
+}
+
+// 读取模块脚本
+std::optional<std::string> JSModuleManager::ReadModuleScript(const std::filesystem::path& module_path) {
+    auto file = std::ifstream(module_path);
+    if (!file) {
+        return std::nullopt;
     }
+    auto content = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    file.close();
+    return content;
+}
 
-    mjs::Value GetModuleAsync(mjs::Context* ctx, std::string_view path) override {
-        return GetModule(ctx, path);
-    }
+mjs::Value JSModuleManager::LoadJSModule(JSService* js_service, std::string_view module_name) {
+    std::filesystem::path path;
+    mjs::Value module;
+    auto& js_module = js_service->js_module();
+    if (!js_module.IsUndefined()) {
+        fs::path cur_module_path = js_module.module_def().name();
 
-    void ClearModuleCache() override {
-        module_defs_.clear();
-    }
-
-private:
-    // 读取模块脚本
-    std::optional<std::string> ReadModuleScript(const std::filesystem::path& module_path) {
-        auto file = std::ifstream(module_path);
-        if (!file) {
-            return std::nullopt;
-        }
-        auto content = std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-        file.close();
-        return content;
-    }
-
-    mjs::Value LoadJSModule(JSService* js_service, std::string_view module_name) {
-        std::filesystem::path path;
-        mjs::Value module;
-        auto& js_module = js_service->js_module();
-        if (!js_module.IsUndefined()) {
-            fs::path cur_module_path = js_module.module_def().name();
-
-            path = cur_module_path.parent_path();
-            assert(path.is_absolute());
-            // 从模块所在路径找模块
-            path /= module_name;
-            path = path.lexically_normal();
-            module = FindJSModule(js_service, path);
-        }
-        
-        if (module.IsUndefined()) {
-            // 当前路径找不到，去配置路径找
-            for (const auto& dir : jssvr_dirs_) {
-                path = dir;
-                path /= module_name;
-                module = FindJSModule(js_service, path);
-                if (!module.IsUndefined()) break;
-            }
-
-            if (!module.IsUndefined()) {
-                return mjs::Value(std::format("LoadModule failed: {}.", module_name));
-            }
-        }
-
-        if (module.IsModuleDef()) {
-            js_service->js_context().CallModule(&module);
-            js_service->js_module_cache().emplace(path, module);
-        }
-        return module;
-    }
-
-    mjs::Value FindJSModule(JSService* js_service, std::filesystem::path path) {
+        path = cur_module_path.parent_path();
         assert(path.is_absolute());
-
-        // 找Context缓存是否存在此模块
-        auto& context_cache = js_service->js_module_cache();
-        auto iter = context_cache.find(path);
-        if (iter != module_defs_.end()) {
-            return iter->second;
-        }
-
-        // 找Runtime缓存的模块定义
-        iter = module_defs_.find(path);
-        if (iter != module_defs_.end()) {
-            return iter->second;
-        }
-
-        auto script = ReadModuleScript(path);
-        if (!script) {
-            return mjs::Value();
-        }
-
-        auto module_def = js_service->js_context().CompileModule(path.string(), *script);
-        module_defs_.emplace(path, module_def);
-        return module_def;
+        // 从模块所在路径找模块
+        path /= module_name;
+        path = path.lexically_normal();
+        module = FindJSModule(js_service, path);
     }
 
-private:
-    std::vector<std::string> jssvr_dirs_;
-    std::unordered_map<fs::path, mjs::Value> cpp_modules_;
-    std::unordered_map<fs::path, mjs::Value> module_defs_;
-};
+    if (module.IsUndefined()) {
+        // 当前路径找不到，去配置路径找
+        auto& jssvr_dirs = js_service->js_runtime_service().jssvr_dirs();
+        for (const auto& dir : jssvr_dirs) {
+            path = fs::absolute(dir);
+            path /= module_name;
+            module = FindJSModule(js_service, path);
+            if (!module.IsUndefined()) break;
+        }
+
+        if (module.IsUndefined()) {
+            return mjs::Value(std::format("LoadModule failed: {}.", module_name));
+        }
+    }
+
+    if (module.IsModuleDef()) {
+        js_service->js_context().CallModule(&module);
+        js_service->js_module_cache().emplace(path, module);
+    }
+    return module;
+}
+
+mjs::Value JSModuleManager::FindJSModule(JSService* js_service, std::filesystem::path path) {
+    assert(path.is_absolute());
+
+    // 再找下Cpp模块
+
+    try {
+        path = fs::canonical(path);
+    }
+    catch (const fs::filesystem_error& e) {
+        return mjs::Value();
+    }
+
+    // 找Context缓存是否存在此模块
+    auto& context_cache = js_service->js_module_cache();
+    auto iter = context_cache.find(path);
+    if (iter != context_cache.end()) {
+        return iter->second;
+    }
+
+    // 找Runtime缓存的模块定义
+    iter = module_defs_.find(path);
+    if (iter != module_defs_.end()) {
+        return iter->second;
+    }
+
+    auto script = ReadModuleScript(path);
+    if (!script) {
+        return mjs::Value();
+    }
+
+    auto module_def = js_service->js_context().CompileModule(path.string(), *script);
+    module_defs_.emplace(path, module_def);
+    return module_def;
+}
 
 
 } // namespace jssvr
