2e7da03925140b6aabe6560ecacdc5b82362dd5e
yuyuaqwq <1454832774@qq.com>
2025-07-29 11:49:18 +0800

    cmake

--- build/build_example.bat
+++ build/build_example.bat
@@ -1,8 +1,7 @@
 cmake .. ^
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
-::    -DProtobuf_ROOT="[protobuf-root-root]" ^
 ::    -DMYSQL_INCLUDE_DIR="[mysql-root]/include" ^
-::    -DMYSQL_LIBRARIES="[mysql-root]/lib/libmysql.lib" ^
+::    -DProtobuf_ROOT="[protobuf-root-root]" ^
 ::    -DOPENSSL_ROOT_DIR="[openssl-root]" ^
     -DCMAKE_TOOLCHAIN_FILE="[vcpkg-root]/scripts/buildsystems/vcpkg.cmake"
 pause
