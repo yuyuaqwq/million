c38960b7207bce1bba2fb78f1646fee827ba9480
yuyuaqwq <1454832774@qq.com>
2025-08-29 18:57:09 +0800

    protobuf, fuck you!

--- build.bat
+++ build.bat
@@ -3,5 +3,5 @@ cd build
 cmake .. ^
     -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
     -DMYSQL_INCLUDE_DIR="C:\Users\Administrator\Desktop\projects\vcpkg\installed\x64-windows\include\mysql" ^
-    -DCMAKE_TOOLCHAIN_FILE="C:/Users/Administrator/Desktop/projects/vcpkg/scripts/buildsystems/vcpkg.cmake"
+    -DCMAKE_TOOLCHAIN_FILE="C:\Users\Administrator\Desktop\projects\vcpkg\scripts\buildsystems\vcpkg.cmake"
 pause
