b8bfc56b195f76a8e034af3e87f82115a0858413
yuyuaqwq <1454832774@qq.com>
2025-10-04 17:29:00 +0800

    vcpkg调整

--- build.bat
+++
@@ -1,7 +0,0 @@
-mkdir build
-cd build
-cmake .. ^
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
-    -DMYSQL_INCLUDE_DIR="D:\project\mmo-pokemon-server2\million\build\vcpkg_installed\x64-windows\include\mysql" ^
-    -DCMAKE_TOOLCHAIN_FILE="C:\src\vcpkg\scripts\buildsystems\vcpkg.cmake"
-pause                 
--- build_example.bat
+++ build_example.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 cmake .. ^
-    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ^
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.15 ^
 ::    -DMYSQL_INCLUDE_DIR="[mysql-root]/include/mysql" ^
     -DCMAKE_TOOLCHAIN_FILE="[vcpkg-root]/scripts/buildsystems/vcpkg.cmake"
 pause
